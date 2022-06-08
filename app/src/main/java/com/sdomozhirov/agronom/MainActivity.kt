5a7f24bac9bcadeb80b504e0650aa3657817b3e7
Sergey Domozhirov <domozhirov86@yandex.ru>
2022-06-08 22:11:44 +0400

    Git

--- app/src/main/java/com/sdomozhirov/agronom/MainActivity.kt
+++ app/src/main/java/com/sdomozhirov/agronom/MainActivity.kt
@@ -2,9 +2,11 @@ package com.sdomozhirov.agronom
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     private var number:Int = 0
+    private val string: String = "beginning"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
